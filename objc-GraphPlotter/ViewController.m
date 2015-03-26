117d43b7852195c0027e7ede85d09aa1f423e21f
zdrossman <zachary.drossman@gmail.com>
2015-03-26 10:29:20 -0400

    Added base project

---
+++ objc-GraphPlotter/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  objc-GraphPlotter
+//
+//  Created by Zachary Drossman on 3/3/15.
+//  Copyright (c) 2015 Zachary Drossman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+
+}
+
+
+@end
